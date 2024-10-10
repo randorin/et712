7e98c5cadd90748ff8f34161ee8c4f8ecd511f93
BetsyZhang0 <160428307+randorin@users.noreply.github.com>
2024-10-10 15:56:20 -0400

    add lab8

---
+++ lab8/events.js
@@ -0,0 +1,7 @@
+let squarediv = document.querySelector('.square')
+squarediv.onclick = function() {
+    squarediv.style.backgroundColor = "magenta"
+}
+
+let animal = document.querySelector('.btn-animal')
+let animalsdiv = document.querySelector('.animalsdiv')
