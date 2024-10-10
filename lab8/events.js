f1417899eaf095c15c005bb0683203dcbe4892e5
BetsyZhang0 <160428307+randorin@users.noreply.github.com>
2024-10-10 16:19:57 -0400

    add some events

--- lab8/events.js
+++ lab8/events.js
@@ -5,3 +5,22 @@ squarediv.onclick = function() {
 
 let animal = document.querySelector('.btn-animal')
 let animalsdiv = document.querySelector('.animalsdiv')
+
+let animals = ['fish', 'dog', 'turtle', 'cat', 'spider']
+
+animal.onclick = function() {
+    animalsdiv.textContent = animals[Math.floor(Math.random()*4)]
+}
+
+let numberdiv = document.querySelector('.numberdiv')
+
+numberdiv.onmouseout = function(){
+    numberdiv.textContent = Math.ceil(Math.random()*99)
+}
+
+function pickcolor() {
+    let red = Math.floor(Math.random()*255)
+    let green = Math.floor(Math.random()*255)
+    let blue = Math.floor(Math.random()*255)
+    return `rgb(${red}, ${green}, ${blue})`
+}
