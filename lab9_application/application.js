d892b71884cab82c397bbaa5f08ef42d08a7693e
BetsyZhang0 <160428307+randorin@users.noreply.github.com>
2024-10-17 15:46:17 -0400

    edit application.js

--- lab9_application/application.js
+++ lab9_application/application.js
@@ -3,5 +3,5 @@ document.addEventListener("keydown", function(event) {
     // prompt ascii code of pressed key
     console.log(`Key ${event.key} was pressed`)
     console.log(`ascii code of the pressed key is ${event.keyCode}`)
-    
+    console.log(`Using 'which' property ${event.which}`)
 })
