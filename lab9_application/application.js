33890f6acf3dd0f18a6de50bfbb3fba22ba16b48
BetsyZhang0 <160428307+randorin@users.noreply.github.com>
2024-10-17 15:44:59 -0400

    key events

---
+++ lab9_application/application.js
@@ -0,0 +1,7 @@
+// key event
+document.addEventListener("keydown", function(event) {
+    // prompt ascii code of pressed key
+    console.log(`Key ${event.key} was pressed`)
+    console.log(`ascii code of the pressed key is ${event.keyCode}`)
+    
+})
