ef9b82ca93c3311a930f88e263f885a0fb4ff899
BetsyZhang0 <160428307+randorin@users.noreply.github.com>
2024-10-25 00:39:46 -0400

    add index.html

---
+++ lab7/index.js
@@ -0,0 +1,5 @@
+document.querySelector('.ps1').textContent = "Betsy Zhang";
+document.querySelectorAll('.ps1')[1].style.fontSize = "25px";
+document.querySelectorAll('.ps1')[1].style.color = "olive";
+document.querySelector('.divs2').style.backgroundColor = "indigo";
+document.querySelectorAll('.divs2')[1].style.width = "300px";
