e3bdb585e997e45296f7001fca48c1eba6aa2e65
BetsyZhang0 <160428307+randorin@users.noreply.github.com>
2024-11-16 22:47:15 -0500

    submit intro to react lab

--- lab12_introreact/src/index.js
+++ lab12_introreact/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import lambo from './images/lambo.jpg';
-
+import './index.css';
 // content
 const App = function() {
+  const anymsg = "I am text constant variable"
   return (
     <>
       <h1 style={{color: 'magenta', textAlign: 'center'}}>Welcome to React JS</h1>
-      <h2 style={{fontFamily: 'algerian'}}>Betsy Zhang</h2>
-      <figure style={{border: 'inset navy 10px'}} >
+      <h2 style={{fontFamily: 'algerian'}} className='subtitle'>Betsy Zhang</h2>
+      <figure style={{border: 'inset navy 10px', width: '60%', margin: 'auto'}} >
         <img src={lambo} style={{width: '100%', display: 'block'}}/>
       </figure>
       <figcaption style={{textAlign: 'center', fontSize: '20px'}} >
         The Lamborghini Sián
       </figcaption>
+      <h2 style={{padding: '2em'}}>{anymsg}</h2>
     </>
   )
 }
